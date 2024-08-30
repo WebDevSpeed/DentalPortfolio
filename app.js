65daa5e154d3b31a7e7457daf1a99b74c3acf00b
WebDevSpeed <webdevspeed@gmail.com>
2024-08-31 02:46:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-const btnText = document.querySelector('#btn-service')
-    const showService = document.querySelector('#hidden');
-    let temp = false;
-    const toggleServices = ()=>{
-        if(!temp){
-            showService.classList.remove('service-box-container-hidden');
-            showService.classList.add('service-box-container');
-            btnText.innerText = "View Less" 
-            temp=true;
-        }
-        else{
-            showService.classList.remove('service-box-container');
-            showService.classList.add('service-box-container-hidden');
-            btnText.innerText = "View All Services"
-            temp=false;
-        }
-    }
+const btnText = document.querySelector("#btn-service");
+const showService = document.querySelector("#hidden");
+let temp = false;
+const toggleServices = () => {
+  if (!temp) {
+    showService.classList.remove("service-box-container-hidden");
+    showService.classList.add("service-box-container");
+    btnText.innerText = "View Less";
+    temp = true;
+  } else {
+    showService.classList.remove("service-box-container");
+    showService.classList.add("service-box-container-hidden");
+    btnText.innerText = "View All Services";
+    temp = false;
+  }
+};
 
-    btnText.addEventListener('click', toggleServices)
+btnText.addEventListener("click", toggleServices);
+
+
+const ham = document.querySelector("#ham");
+const hamburger = document.querySelector(".hamburger");
+let hide = false;
+const toggleHamburger = ()=>{
+    if(!hide){
+        console.log("inside false");
+        
+        ham.classList.remove("ham-hide");
+        ham.classList.add("ham-visible");
+        hide = true;
+    }else{
+        console.log("inside true");
+        
+        ham.classList.remove("ham-visible");
+        ham.classList.add("ham-hide");
+        hide = false;
+    }
+}
+
+hamburger.addEventListener("click",toggleHamburger);
